696aa58eac64cdcb19cb7db23fe8b6a76a331df8
“didi-gg” <marcela.gomezg02@gmail.com>
2025-06-28 15:57:30 -0500

    generate only one process student_modules dataset

--- scripts/transform/process_student_modules.py
+++ scripts/transform/process_student_modules.py
@@ -18,13 +18,13 @@ class StudentModulesProcessor(BaseScript):
 
     @staticmethod
     def _merge_modules_students(students_df, modules_df):
-        students_sel = students_df[["moodle_user_id", "documento_identificación", "year", "course_id", "sede"]]
-        return students_sel.merge(modules_df, on=["course_id", "year", "sede"], how="inner")
+        students_sel = students_df[["documento_identificación","year","id_grado","sede"]]
+        return students_sel.merge(modules_df, on=["id_grado", "year", "sede"], how="inner")
 
     @staticmethod
     def _get_student_logs_summary(logs_df):
         student_logs_summary = (
-            logs_df.groupby(["userid", "contextinstanceid", "year"])
+            logs_df.groupby(["documento_identificación", "contextinstanceid", "year", "platform"])
             .agg(
                 num_views=("event_type", lambda x: (x == "view").sum()),
                 num_interactions=("event_type", lambda x: (x == "interaction").sum()),
@@ -32,7 +32,7 @@ class StudentModulesProcessor(BaseScript):
                 last_view=("timecreated", "max"),
             )
             .reset_index()
-            .rename(columns={"userid": "moodle_user_id", "contextinstanceid": "course_module_id"})
+            .rename(columns={"contextinstanceid": "course_module_id"})
         )
         return student_logs_summary
 
@@ -57,6 +57,7 @@ class StudentModulesProcessor(BaseScript):
             [
                 "year",
                 "course_id",
+                "platform",
                 "course_module_id",
                 "sede",
                 "id_grado",
@@ -80,31 +81,25 @@ class StudentModulesProcessor(BaseScript):
         ]
 
         df_base = StudentModulesProcessor._merge_modules_students(students_df, modules_df)
-        logs_df["event_type"] = logs_df["eventname"].apply(StudentModulesProcessor._classify_event)
-        student_logs_summary = StudentModulesProcessor._get_student_logs_summary(logs_df)
-        df_full = df_base.merge(student_logs_summary, on=["moodle_user_id", "course_module_id", "year"], how="left")
-        df_full = StudentModulesProcessor._calculate_metrics(df_full)
 
+        logs_df["event_type"] = logs_df["eventname"].apply(StudentModulesProcessor._classify_event)
+
+        student_logs_summary = StudentModulesProcessor._get_student_logs_summary(logs_df)
+
+        df_full = df_base.merge(student_logs_summary, on=["documento_identificación", "course_module_id", "year", "platform"], how="left")
+
+        df_full = StudentModulesProcessor._calculate_metrics(df_full)
         return df_full
 
     def process_course_data(self):
-        modules_df = pd.read_csv("data/interim/moodle/modules_featured_moodle.csv")
-        students_df = pd.read_csv("data/interim/moodle/student_courses_moodle.csv")
-        logs_df = pd.read_csv("data/interim/moodle/student_logs_moodle.csv")
+        modules_df = pd.read_csv("data/interim/moodle/modules_featured.csv")
+        students_df = pd.read_csv("data/interim/estudiantes/enrollments.csv")
+        logs_df = pd.read_csv("data/interim/moodle/student_logs.csv")
 
         student_modules_moodle = StudentModulesProcessor._process_df(modules_df, students_df, logs_df)
 
-        modules_df = pd.read_csv("data/interim/moodle/modules_featured_edukrea.csv")
-        students_df = pd.read_csv("data/interim/moodle/student_courses_edukrea.csv")
-        logs_df = pd.read_csv("data/interim/moodle/student_logs_edukrea.csv")
-
-        student_modules_edukrea = StudentModulesProcessor._process_df(modules_df, students_df, logs_df)
-
-        self.save_to_csv(student_modules_moodle, "data/interim/moodle/student_modules_moodle.csv")
-        self.save_to_csv(student_modules_edukrea, "data/interim/moodle/student_modules_edukrea.csv")
-
+        self.save_to_csv(student_modules_moodle, "data/interim/moodle/student_modules.csv")
         self.logger.info("Courses data processed and saved successfully.")
-
 
 if __name__ == "__main__":
     processor = StudentModulesProcessor()
